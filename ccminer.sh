6b42b126e9a70851d5089e2434bbd9fcae4298e9
tourniquetrules <tourniquetrules@gmail.com>
2024-11-29 07:25:07 -0600

    Update ccminer.sh

--- ccminer.sh
+++ ccminer.sh
@@ -3,4 +3,4 @@ apt install zip nano -y
 mkdir ccminer
 wget -P ccminer https://github.com/1NF1N18Y/ccminer-Points/releases/download/v0.1-beta/test-release.zip &&
 unzip ccminer/test-release.zip -d ccminer
-chmod +x /ccminer/ccminer
+chmod +x ccminer/ccminer
